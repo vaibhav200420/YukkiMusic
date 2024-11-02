b08a82b1e755cbc9d636e3dd03708ff28b3c0e6a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-11-02 08:19:43 +0530

    playlist in telethon

--- YukkiMusic/utils/inline/playlist.py
+++ YukkiMusic/utils/inline/playlist.py
@@ -7,112 +7,89 @@
 #
 # All rights reserved.
 #
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from telethon import Button
 
 def botplaylist_markup(_):
     buttons = [
         [
-            InlineKeyboardButton(
+            Button.inline(
                 text=_["PL_B_1"],
-                callback_data="get_playlist_playmode",
+                data="get_playlist_playmode",
             ),
-            InlineKeyboardButton(text=_["PL_B_8"], callback_data="get_top_playlists"),
+            Button.inline(text=_["PL_B_8"], data="get_top_playlists"),
         ],
         [
-            InlineKeyboardButton(text=_["PL_B_4"], callback_data="PM"),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            Button.inline(text=_["PL_B_4"], data="PM"),
+            Button.inline(text=_["CLOSE_BUTTON"], data="close"),
         ],
     ]
     return buttons
-
 
 def top_play_markup(_):
     buttons = [
-        [InlineKeyboardButton(text=_["PL_B_9"], callback_data="SERVERTOP global")],
-        [InlineKeyboardButton(text=_["PL_B_10"], callback_data="SERVERTOP chat")],
-        [InlineKeyboardButton(text=_["PL_B_11"], callback_data="SERVERTOP user")],
+        [Button.inline(text=_["PL_B_9"], data="SERVERTOP global")],
+        [Button.inline(text=_["PL_B_10"], data="SERVERTOP chat")],
+        [Button.inline(text=_["PL_B_11"], data="SERVERTOP user")],
         [
-            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="get_playmarkup"),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            Button.inline(text=_["BACK_BUTTON"], data="get_playmarkup"),
+            Button.inline(text=_["CLOSE_BUTTON"], data="close"),
         ],
     ]
     return buttons
-
 
 def get_playlist_markup(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["P_B_1"], callback_data="play_playlist a"),
-            InlineKeyboardButton(text=_["P_B_2"], callback_data="play_playlist v"),
+            Button.inline(text=_["P_B_1"], data="play_playlist a"),
+            Button.inline(text=_["P_B_2"], data="play_playlist v"),
         ],
         [
-            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="home_play"),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            Button.inline(text=_["BACK_BUTTON"], data="home_play"),
+            Button.inline(text=_["CLOSE_BUTTON"], data="close"),
         ],
     ]
     return buttons
-
-
-def top_play_markup(_):
-    buttons = [
-        [InlineKeyboardButton(text=_["PL_B_9"], callback_data="SERVERTOP Global")],
-        [InlineKeyboardButton(text=_["PL_B_10"], callback_data="SERVERTOP Group")],
-        [InlineKeyboardButton(text=_["PL_B_11"], callback_data="SERVERTOP Personal")],
-        [
-            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="get_playmarkup"),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
-        ],
-    ]
-    return buttons
-
 
 def failed_top_markup(_):
     buttons = [
         [
-            InlineKeyboardButton(
+            Button.inline(
                 text=_["BACK_BUTTON"],
-                callback_data="get_top_playlists",
+                data="get_top_playlists",
             ),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            Button.inline(text=_["CLOSE_BUTTON"], data="close"),
         ],
     ]
     return buttons
 
-
 def warning_markup(_):
-    upl = InlineKeyboardMarkup(
+    buttons = [
         [
-            [
-                InlineKeyboardButton(
-                    text=_["PL_B_7"],
-                    callback_data="delete_whole_playlist",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["BACK_BUTTON"],
-                    callback_data="del_back_playlist",
-                ),
-                InlineKeyboardButton(
-                    text=_["CLOSE_BUTTON"],
-                    callback_data="close",
-                ),
-            ],
-        ]
-    )
-    return upl
-
+            Button.inline(
+                text=_["PL_B_7"],
+                data="delete_whole_playlist",
+            ),
+        ],
+        [
+            Button.inline(
+                text=_["BACK_BUTTON"],
+                data="del_back_playlist",
+            ),
+            Button.inline(
+                text=_["CLOSE_BUTTON"],
+                data="close",
+            ),
+        ],
+    ]
+    return buttons
 
 def close_markup(_):
-    upl = InlineKeyboardMarkup(
+    buttons = [
         [
-            [
-                InlineKeyboardButton(
-                    text=_["CLOSE_BUTTON"],
-                    callback_data="close",
-                ),
-            ]
+            Button.inline(
+                text=_["CLOSE_BUTTON"],
+                data="close",
+            ),
         ]
-    )
-    return upl
+    ]
+    return buttons
