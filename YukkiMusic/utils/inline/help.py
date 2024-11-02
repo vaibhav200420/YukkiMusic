435f0b282bc2124e7b13d17d9377e53750ea9c75
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-11-02 08:09:44 +0530

    help in telethon

--- YukkiMusic/utils/inline/help.py
+++ YukkiMusic/utils/inline/help.py
@@ -7,44 +7,36 @@
 #
 # All rights reserved.
 #
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from telethon import Button
 from config import SUPPORT_GROUP
 from YukkiMusic import app
 
-
 def support_group_markup(_):
-    upl = InlineKeyboardMarkup(
+    upl = [
         [
-            [
-                InlineKeyboardButton(
-                    text=_["S_B_3"],
-                    url=SUPPORT_GROUP,
-                ),
-            ]
+            Button.url(
+                text=_["S_B_3"],
+                url=SUPPORT_GROUP,
+            ),
         ]
-    )
+    ]
     return upl
-
 
 def help_back_markup(_):
-    upl = InlineKeyboardMarkup(
+    upl = [
         [
-            [
-                InlineKeyboardButton(
-                    text=_["BACK_BUTTON"], callback_data=f"settings_back_helper"
-                ),
-                InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close"),
-            ]
+            Button.inline(
+                text=_["BACK_BUTTON"], data="settings_back_helper"
+            ),
+            Button.inline(text=_["CLOSE_BUTTON"], data="close"),
         ]
-    )
+    ]
     return upl
-
 
 def private_help_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
+            Button.url(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?start=help"
             )
         ],
